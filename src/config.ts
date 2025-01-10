59aee450c6a21bb72ac7e5b1703dcd894d550b63
Dustin Do <dustin.do95@gmail.com>
2025-01-11 02:02:28 +0700

    Allow config CAMERAS

--- src/config.ts
+++ src/config.ts
@@ -13,13 +13,14 @@ if (!parsed) {
 
 const parsedEnvs = parsed! as {
   FLEET_URL: string
+  CAMERAS: string
   FLEET_TOKEN?: string
   DATA_PATH?: string
   TELEGRAM_BOT_TOKEN?: string
   TELEGRAM_CHAT_ID?: string
 }
 
-const CAMERAS = ['fcamera', 'ecamera', 'dcamera'] as const
+const CAMERAS = parsedEnvs.CAMERAS?.split(',') || ['ecamera', 'dcamera']
 const DATA_PATH = parsedEnvs.DATA_PATH || join(process.cwd(), 'data')
 const DB_PATH = join(DATA_PATH, 'db.json')
 const VIDEOS_PATH = join(DATA_PATH, 'videos')
